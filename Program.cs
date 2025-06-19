769a1a02d9ef0c077e042965f1bc070525cb9d4f
PL.D De Silva <169179183+DilshanDe@users.noreply.github.com>
2025-06-19 11:19:11 +0530

    Initial commit

--- Program.cs
+++ Program.cs
@@ -2,13 +2,36 @@
 
 namespace result
 {
-    class program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(3 + 4);
-            Console.Write("Hello");
-            Console.WriteLine(5 + 6);
+            int num1;
+            int num2;
+            int result;
+
+            Console.WriteLine("Enter your Frist Number: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Your Second Number: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            //Addtion
+            result = (num1 + num2);
+            Console.WriteLine("Addition: " + result);
+
+            //subtraction
+            result = (num1 - num2);
+            Console.WriteLine("Subtraction: " + result);
+
+            //Multipication
+            result = (num1 * num2);
+            Console.WriteLine("Multipication: " + result);
+
+            //Division
+            result = (num1 / num2);
+            Console.WriteLine("Division: " + result);
+
             Console.ReadLine();
         }
     }
