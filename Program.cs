fff33ceeba3ea0f21eec1d94df7f181128b1db12
PL.D De Silva <169179183+DilshanDe@users.noreply.github.com>
2025-06-19 11:16:00 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace result
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(3 + 4);
+            Console.Write("Hello");
+            Console.WriteLine(5 + 6);
+            Console.ReadLine();
+        }
+    }
+}
